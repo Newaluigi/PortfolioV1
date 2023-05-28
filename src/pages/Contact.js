10c111d306e420823723fb2a1107130669c99602
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-05-29 01:57:13 +0200

    contact page finished

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,77 @@
 import React from "react";
 import Navigation from "../components/Navigation";
+import {
+  FaEnvelope,
+  FaGithub,
+  FaLinkedin,
+  FaMapMarkerAlt,
+  FaMobileAlt,
+} from "react-icons/fa";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact">
       <Navigation />
-      Contact moi
+      <div className="contactContent">
+        <div className="header"></div>
+        <div className="contactBox">
+          <h1>Contactez-moi</h1>
+          <ul>
+            <li>
+              <FaMapMarkerAlt />
+              <span>Boulogne-sur-mer</span>
+            </li>
+            <li>
+              <FaMobileAlt />
+              <CopyToClipboard text="+33 6 61 17 24 87">
+                <span
+                  className="clickInput"
+                  onClick={() => {
+                    alert("Téléphone copié !");
+                  }}
+                >
+                  +33 6 61 17 24 87
+                </span>
+              </CopyToClipboard>
+            </li>
+            <li>
+              <FaEnvelope />
+              <CopyToClipboard text="alexandre.levilain@hotmail.com">
+                <span
+                  className="clickInput"
+                  onClick={() => {
+                    alert("E-mail copié !");
+                  }}
+                >
+                  alexandre.levilain@hotmail.com
+                </span>
+              </CopyToClipboard>
+            </li>
+          </ul>
+        </div>
+        <div className="socialNetwork">
+          <ul>
+            <a
+              href="https://www.linkedin.com/in/alexandrelevillain/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h4>LinkedIn</h4>
+              <FaLinkedin />
+            </a>
+            <a
+              href="https://github.com/Newaluigi"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="contsocial"
+            >
+              <h4>Github</h4>
+              <FaGithub />
+            </a>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
